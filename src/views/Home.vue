8cd8e22d3e609185d7ed09759b9c71b20667667f
huanganxuan <1039215157@qq.com>
2020-09-02 15:43:46 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top">无人机光伏巡检管理系统</div>
+    <div class="center">
+      <div>center</div>
+      <div>center</div>
+      <div>center</div>
+      <div>center</div>
+      <div>center</div>
+      <div>center</div>
+    </div>
+    <div class="bottom">bottom</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style lang="less" scoped>
+.home {
+  width: 100%;
+  height: 600px;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: space-around;
+  .top {
+    text-align: center;
+    line-height: 28px;
+    height: 28px;
+    font-size: 28px;
+    font-family: PingFang SC;
+    font-weight: 500;
+    color: #197df8;
+  }
+  // .center {
+  //   flex: 1;
+  // }
+  // .bottom {
+  //   flex: 1;
+  // }
+}
+</style>
