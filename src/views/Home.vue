b3293c60939c931bfa69930d19f6207fe5df6ff0
huanganxuan <1039215157@qq.com>
2020-09-02 17:40:52 +0800

    bottom

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,34 @@
   <div class="home">
     <div class="top">无人机光伏巡检管理系统</div>
     <div class="center">
+      <div class="field-title">用户名</div>
       <van-field v-model="value" placeholder="请输入用户名" :border="false" />
-      <van-field v-model="value" placeholder="请输入用户名" :border="false" />
-      <van-button type="info" block round>登 录</van-button>
+      <div class="field-title">密码</div>
+      <van-field v-model="value" placeholder="请输入短信验证码" center clearable :border="false">
+        <template #button>
+          <van-button size="small" color="#FA7E05">发送验证码</van-button>
+        </template>
+      </van-field>
+      <van-button type="info" block>登 录</van-button>
     </div>
-    <div class="bottom">bottom</div>
+    <div class="bottom">
+      <div class="bottom-title">——— 第三方登录 ———</div>
+      <div>
+        <van-grid :gutter="10" :border="false">
+          <van-grid-item>
+            <van-image src="@/assets/wechart.png" round />
+          </van-grid-item>
+          <!-- <van-grid-item>
+            <van-image src="https://img.yzcdn.cn/vant/apple-1.jpg" round fit="cover" />
+          </van-grid-item>-->
+        </van-grid>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { Field, Button } from "vant";
+import { Field, Button, Grid, GridItem, Image } from "vant";
 export default {
   data() {
     return {
@@ -21,6 +39,9 @@ export default {
   components: {
     [Field.name]: Field,
     [Button.name]: Button,
+    [Grid.name]: Grid,
+    [GridItem.name]: GridItem,
+    [Image.name]: Image,
   },
 };
 </script>
@@ -49,6 +70,7 @@ export default {
     /deep/ .van-cell {
       background-color: transparent;
       margin-bottom: 15px;
+      padding: 5px 15px;
     }
     /deep/ .van-field__control {
       border-bottom: 1px solid #d5d5d5;
@@ -56,6 +78,25 @@ export default {
     /deep/ .van-button--block {
       margin: 0 10px;
       width: calc(100% - 20px);
+    }
+    .field-title {
+      font-size: 16px;
+      font-weight: bold;
+      padding: 0 15px;
+      font-family: PingFang SC;
+      font-weight: 500;
+      color: #222222;
+    }
+  }
+  .bottom {
+    text-align: center;
+    .bottom-title {
+      height: 12px;
+      font-size: 12px;
+      font-family: PingFang SC;
+      font-weight: 500;
+      color: #888888;
+      line-height: 24px;
     }
   }
 }
